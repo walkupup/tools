0926ff878fc07f3c84b7a7f4ff3fcaa03cdc5830
walkupup <yl99@163.com>
2017-05-13 09:31:44 +0800

    modify

--- VideoCapTest/videocap.cpp
+++ VideoCapTest/videocap.cpp
@@ -5,14 +5,15 @@ using namespace cv;
 
 int main(int, char**)
 {
-	int width = 720;
-	int height = 576;
+	int width = 640;
+	int height = 480;
+	int fps = 5;
 	int frameNum = 0;
-	VideoCapture cap(0); // open the default camera
+	VideoCapture cap(0); // open the default camera"E:\\data\\recored-hd\\32west1-1.mp4"
 	Mat frame;
 	std::string videoName = "d:\\1.avi";
-	cap.set(CV_CAP_PROP_FRAME_WIDTH, 720);
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 576);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, width);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, height);
 	printf("width = %d, height = %d\n", (int)cap.get(CV_CAP_PROP_FRAME_WIDTH), (int)cap.get(CV_CAP_PROP_FRAME_HEIGHT));
 	printf("framerate = %f, format = %f\n", cap.get(CV_CAP_PROP_FPS), cap.get(CV_CAP_PROP_FORMAT));
 
@@ -20,12 +21,17 @@ int main(int, char**)
 	VideoWriter save(videoName, -1, 15, Size(frame.cols, frame.rows));
 	if (!cap.isOpened()) // check if we succeeded
 		return -1;
-	namedWindow("capture", 1);
+	//namedWindow("capture", 1);
 	printf("start recording to %s, press c to stop\n", videoName.c_str());
 
-	for (;;)
+	for (int i = 0; i < 10000; i++)
 	{
 		cap >> frame; // get a new frame from camera
+		if (frame.empty())
+		{
+			printf("error\n");
+			continue;
+		}
 		save << frame;
 		imshow("capture", frame);
 		printf("frame %d\n", frameNum++);
