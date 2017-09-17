0bddeac2ae9ed89e6b0df8421d348aed6d6718a0
walkup <yl99@163.com>
2017-09-18 01:03:27 +0800

    image list

--- PythonTool/image_list_id.py
+++ PythonTool/image_list_id.py
@@ -2,6 +2,7 @@
 
 # 给定根目录，生成子目录下的图像列表和标签，用于caffe图像分类训练.
 import os
+import random
 def image_list_id(imageRoot):
     label = 0
     for filename in os.listdir(imageRoot):
@@ -14,4 +15,16 @@ def image_list_id(imageRoot):
                         f.write('%s %d\n' %((filename + '/' + imagename), label))
             label += 1
 
-image_list_id(r'D:\data\vr\siamese')
+def shuffle_split(listFile, trainFile, valFile):
+    with open(listFile, 'r') as f:
+        records = f.readlines()
+    random.shuffle(records)
+    num = len(records)
+    trainNum = int(num * 0.8)
+    with open(trainFile, 'w') as f:
+        f.writelines(records[0:trainNum])
+    with open(valFile, 'w') as f1:
+        f1.writelines(records[trainNum:])
+
+#image_list_id(r'D:\data\vr\siamese')
+shuffle_split('list.txt', 'train.txt', 'val.txt')
